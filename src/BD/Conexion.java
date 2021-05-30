5a360ad41646a476dde266471b89562dc92f8023
PabloBarrionuevo <82464280+PabloBarrionuevo@users.noreply.github.com>
2021-05-30 00:08:22 -0300

    Main para pruebas, Conexion, Vista Principal

---
+++ src/BD/Conexion.java
@@ -0,0 +1,101 @@
+/*
+ * Clase de Conexion a la BD
+ */
+package BD;
+
+/**
+ * La Conexion se establece usando XAMPP con MariaDB
+ * @author Astor, Meli, Bruno, Pablo
+ */
+public class Conexion {
+    /**
+     * Configuraciones predeterminadas de acceso
+     */
+    private static final class CONF{
+        static final String DRIVER = "org.mariadb.jdbc.Driver";
+        static final String DIR = "localhost";
+        static final String BD = "Biblioteca";
+        static final String PUERTO = "3306";
+        static final String USUARIO = "root";
+        static final String PASS = "";
+    }
+    //Atributos
+    private String url;
+    private String usuario;
+    private String pass;
+    private java.sql.Connection conexion = null;
+    
+    //Constructores
+    /**
+     * Arma y crea una Conexion con datos predeterminados
+     * url = "jdbc:mariadb://localhost:3306/Biblioteca"
+     * usuario = "root"
+     * contraseña = ""
+     */
+    public Conexion() {
+        url = "jdbc:mariadb://"+ CONF.DIR +":"+ CONF.PUERTO +"/"+ CONF.BD;
+        usuario = CONF.USUARIO;
+        pass = CONF.PASS;
+        registrarDriver();
+        System.out.println("Nueva Conexion:\n"
+                + "URL: "+ url +".\n"
+                + "Usuario: "+ usuario +".\n"
+                + "Password: "+ pass +".\n");
+    }
+    /**
+     * Arma y crea una Conexion a la BD
+     * @param nombreBD el nombre de la Base de Datos
+     * @param usuario
+     * @param pass 
+     */
+    public Conexion(String nombreBD, String usuario, String pass) {
+        url = "jdbc:mariadb://"+ CONF.DIR +":"+ CONF.PUERTO +"/"+ nombreBD;
+        this.usuario = usuario;
+        this.pass = pass;
+        System.out.println("Nueva Conexion:\n"
+                + "URL: "+ url +".\n"
+                + "Usuario: "+ usuario +".\n"
+                + "Password: "+ pass +".\n");
+        registrarDriver();
+    }
+    //Getter de conecction
+    /**
+     * Devuelve una java.sql.Connection o null si error al ejecutar conectar().
+     * @return 
+     */
+    public java.sql.Connection getConexion(){
+        return conexion;
+    }
+    
+    //Metodos publicos
+    /**
+     * Intenta establecer una conexion a la BD, devuelve null si no es posible
+     * @return 
+     */
+    public java.sql.Connection Conectar(){
+        if(conexion == null){
+            try {
+                conexion = java.sql.DriverManager.getConnection( url + "?useLegacyDatetimeCode=false&serverTimezone=UTC"// + "&user"+ usuario + "&password=" +pass
+                        ,usuario, pass);
+            } catch (java.sql.SQLException ex) {
+                javax.swing.JOptionPane.showMessageDialog(null, "Error al Conectar: \n"+ex);
+                System.out.println("Error al Conectar: "+ex);
+                conexion = null;
+            }
+        }
+        return conexion;
+    }
+    
+    //Metodos privados
+    /**
+     * Metodo utilizado desde los constructores, registra el driver de mariadb en la MVJava
+     */
+    private void registrarDriver(){
+        try {
+            Class.forName(CONF.DRIVER);
+        } catch (ClassNotFoundException ex) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Error de libreria: \n"+ex);
+            System.out.println("Error al registrar Driver: "+ ex);
+        }
+    }
+}
--- src/grupo1tpfinal/Grupo1TPFinal.java
+++ src/grupo1tpfinal/Grupo1TPFinal.java
@@ -1,7 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * TUDS
+ * Laboratorio 1
+ * Trabajo Practico Final
+ * Grupo 1:
+ * Astor Arico
+ * Gianfranco Bruno
+ * Di-Mauro Melisa
+ * Barrionuevo Pablo
  */
 package grupo1tpfinal;
 //asd
@@ -11,12 +16,42 @@ package grupo1tpfinal;
  * @author giani
  */
 public class Grupo1TPFinal {
-
+    
+    //Variables static
+    public static BD.Conexion CONEXION;
+    
+    //Atributos
+    
+    
     /**
+     * Metodo main argumentos no usamos por el momento.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Laboratorio 1: Trabajo Práctico Final.\nGrupo 1.");
+        /*......................................................................//Inicio de Pruebas
+        
+        System.out.println("Prueba Logica:\n");//...............................Probar la Logica
+        //Libro
+        
+        //Ejemplares o Biblioteca
+        
+        //...
+        
+        System.out.println("Prueba de Conexion:\n");//..........................Probar la parte de conexion
+        CONEXION = new BD.Conexion();//("Biblioteca", "root", "");
+        if(CONEXION.Conectar() != null){
+            System.out.println("Conexion OK");
+            System.out.println("Prueba de Clases Data");//......................Probar Clases Data
+            //Libros
+            //...
+        }else{
+            System.out.println("Conexion Error");
+        }
+        ......................................................................*///Fin Pruebas
+        //inicializaciones, preparaciones antes de iniciar GUI
+        CONEXION = new BD.Conexion();
+        GUI.Principal.iniciar();//..............................................GUI
     }
     
 }
