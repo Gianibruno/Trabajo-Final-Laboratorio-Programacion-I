b5a1b94f3044b74fb8d12eeb85382ed1dcb79075
Astor Aricó <82976048+AstorArico72@users.noreply.github.com>
2021-06-03 21:24:53 -0300

    Añadido un int de la deuda
    Y un método void en la clase Ejemplar va a cambiar éso, que a su vez se ejecuta si hay un booleano que se cumple, es decir, si el libro está atrasado.

--- src/Entidades/Lector.java
+++ src/Entidades/Lector.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 public class Lector extends Persona {
 
     private ArrayList<Ejemplar> librosPrestados;
+    private int deuda;
 
     public Lector(long dni, String nombre, String nacionalidad, LocalDate fechaNacimiento) {
         super(dni, nombre, nacionalidad, fechaNacimiento);
