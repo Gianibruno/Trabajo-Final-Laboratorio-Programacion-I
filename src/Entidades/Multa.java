a5d19ade97334b0b50d76dcd543e1fa757ef074d
melid <melid@DESKTOP-F9HLGMH>
2021-06-17 02:41:53 -0300

    Multa

--- src/Entidades/Multa.java
+++ src/Entidades/Multa.java
@@ -16,20 +16,34 @@ public class Multa {
   private int id_multa;
     private LocalDate fecha_inicio;
     private LocalDate fecha_fin;
+    private Prestamo prestamo;
 
     public Multa() {
     }
 
-    public Multa(LocalDate fecha_inicio, LocalDate fecha_fin) {
+    public Multa(Prestamo prestamo, LocalDate fecha_inicio, LocalDate fecha_fin ) {
         this.fecha_inicio = fecha_inicio;
         this.fecha_fin = fecha_fin;
+        this.prestamo = prestamo;
     }
 
-    public Multa(int id_multa, LocalDate fecha_inicio, LocalDate fecha_fin) {
+    public Multa(int id_multa, Prestamo prestamo, LocalDate fecha_inicio, LocalDate fecha_fin) {
         this.id_multa = id_multa;
         this.fecha_inicio = fecha_inicio;
         this.fecha_fin = fecha_fin;
+        this.prestamo = prestamo;
+        
     }
+
+    public Prestamo getPrestamo() {
+        return prestamo;
+    }
+
+    public void setPrestamo(Prestamo prestamo) {
+        this.prestamo = prestamo;
+    }
+    
+    
 
     public int getId_multa() {
         return id_multa;
@@ -54,9 +68,10 @@ public class Multa {
     public void setFecha_fin(LocalDate fecha_fin) {
         this.fecha_fin = fecha_fin;
     }
-    
-    
-    
-   
 
-}
+    @Override
+    public String toString() {
+        return " Multa del Lector: " + getPrestamo().getLector().getNombre() + " del ejemplar del libro: "+ getPrestamo().getEjemplar().getLibro().getNombre(); 
+    }
+    
+  }
